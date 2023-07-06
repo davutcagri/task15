70f58a1837b1e98ee5b7c929684d5b968642c53c
davutcagri <71286320+davutcagri@users.noreply.github.com>
2023-07-06 15:07:19 +0300

    Esitlik durumu

--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,30 @@ public class Main {
         int counter;
         int howMuch = 0;
         int index = 0;
+        int equalsCharCounts = 0;
 
-        System.out.println("Kelime: ");
+        System.out.print("Kelime: ");
         String word = scanner.nextLine();
 
-        for(int i = 0; i < word.length(); i++) {
+        for (int i = 0; i < word.length(); i++) {
             counter = 0;
-            for(int j = 0; j < word.length(); j++) {
-                if(word.charAt(i) == word.charAt(j)) {
+            for (int j = 0; j < word.length(); j++) {
+                if ((word.charAt(i) == word.charAt(j)) && (index != i)) {
                     counter++;
                 }
-                if(counter > howMuch) {
-                    howMuch = counter;
-                    index = i;
-                }
+            }
+            if (counter > howMuch) {
+                howMuch = counter;
+                index = i;
+            }
+            else if (counter == howMuch) {
+                equalsCharCounts = counter;
             }
         }
-        System.out.println(word.charAt(index) + " harfinden " + howMuch + " tane var.");
+        if (howMuch > equalsCharCounts) {
+            System.out.println(word.charAt(index) + " harfinden " + howMuch + " tane var.");
+        } else {
+            System.out.println("Eşitlik durumu mevcut.");
+        }
     }
 }
